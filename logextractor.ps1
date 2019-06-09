93989d45f1a0521c5619cbcaa7878de803ce421c
ankitjoshi8518 <51059319+ankitjoshi8518@users.noreply.github.com>
2019-06-09 20:44:34 +0400

    added support for multiple source folders
    Multiple source folders can be specified in variable and the logs can be pulled based to destination folder in separate sub folders

--- logextractor.ps1
+++ logextractor.ps1
@@ -1,12 +1,46 @@
-$src_log="C:\Users\admin\Desktop\LOG-Extractor\logssrc"
-$src_dest="C:\Users\admin\Desktop\LOG-Extractor\logsdst"
-$findlogsafter=[datetime]"05/07/2019 01:00:00 PM"
-$findloglastdate=[datetime]"05/08/2019 23:00:00 PM"
 
-$filestocopy = Get-ChildItem -Path $src_log | where {$_.LastWriteTime -gt $findlogsafter -AND $_.LastWriteTime -lt $findloglastdate}
+$source=("C:\Users\Administrator\Desktop\logfolder1", "C:\Users\Administrator\Desktop\logfolder2","C:\Users\Administrator\Desktop\logfolder3","C:\Users\Administrator\Desktop\logfolder4" ,"")        #keep "" at the end..specify multiple paths before it
+$destinationrootfolder="C:\Users\Administrator\Desktop\rootdestfolder"
+$findlogsafter=[datetime]"06/06/2019 01:00:00 AM" 
+$findloglastdate=[datetime]"06/09/2019 01:59:00 PM" 
 
-cd $source
+#saving the number of source folders
 
-Copy-Item -Path $filestocopy[0] -Destination $src_dest
+$numberofsourcefolders=($source.Length - 1)                                 #ignore the last null
+$count=0 
+
+
+while ($count -lt $numberofsourcefolders) {
+
+
+#Creating subfolder in the destination 
+
+$folders=$source[$count].Split('\')                                     #Get the number of folders from source path
+$val=$folders.length                                                    # find the length 
+$destinationsubfolder=$folders[$val-1]                                  #get the last folder from source path
+$dest= $destinationrootfolder + '\' + $destinationsubfolder             #save the new destination location
+"Creating New Subfolder in the destination Path"
+mkdir $dest                                                             #create subfolder in root destination folder
+
+$filestocopy = Get-ChildItem -Path $source[$count] | where {$_.LastWriteTime -gt $findlogsafter -AND $_.LastWriteTime -lt $findloglastdate} 
+ 
+$len=$filestocopy.Length
+
+"Number of Files to be copied"
+$len
+$i=0
+
+while ($i -lt $len) {
+
+cd $source[$count]
+Copy-Item -Path $filestocopy[$i] -Destination $dest 
+$i=$i+1
+
+}
+
+
+$count++
+
+}
 
 
